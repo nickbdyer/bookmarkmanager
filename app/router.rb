8749ffd238875b3664d77535fb25191cc18120a2
Nick Dyer <nbdyer@gmail.com>
2014-11-17 17:59:03 +0000

    User adds a new link with a few tags

--- app/router.rb
+++ app/router.rb
@@ -22,7 +22,8 @@ class BookmarkManager < Sinatra::Base
   post '/links' do
     url = params["url"]
     title = params["title"]
-    Link.create(:url => url, :title => title)
+    tags = params["tags"].split(" ").map { |tag| Tag.first_or_create(:text => tag) }
+    Link.create(:url => url, :title => title, :tags => tags)
     redirect to('/')
   end
 
