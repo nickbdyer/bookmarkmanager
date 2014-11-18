fe6f5e4ca090105687542980a347b0d3c355155f
Nick Dyer <nbdyer@gmail.com>
2014-11-18 12:31:07 +0000

    User signs up while they are logged out

--- app/router.rb
+++ app/router.rb
@@ -6,13 +6,24 @@ require 'data_mapper'
   DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
   require './lib/link'
+  require './lib/tag'
+  require './lib/user'
 
   DataMapper.finalize
 
   DataMapper.auto_upgrade!
 
+
 class BookmarkManager < Sinatra::Base
 
+  enable :sessions
+  set :session_secret, 'super secret'
+
+  helpers do
+    def current_user
+      @current_user ||=User.get(session[:user_id]) if session[:user_id]
+    end
+  end
 
   get '/' do
     @links = Link.all
@@ -32,6 +43,18 @@ class BookmarkManager < Sinatra::Base
     @links = tag ? tag.links : []
     erb :index
   end
+
+  get '/users/new' do
+    erb :"users/new"
+  end
+
+  post '/users' do
+    user = User.create(:email => params[:email],
+                :password => params[:password])
+    session[:user_id] = user.id
+    redirect to('/')
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
